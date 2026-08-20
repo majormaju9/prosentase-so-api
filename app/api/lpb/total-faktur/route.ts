ba1e9ffc278b7a3cc2a41b5dbc597968c4f61aec
majormaju9 <majormaju9@gmail.com>
2026-08-21 03:16:08 +0700

    Update route.ts

--- app/api/lpb/total-faktur/route.ts
+++ app/api/lpb/total-faktur/route.ts
@@ -1,113 +1,49 @@
-import { NextRequest, NextResponse } from "next/server";
+const response = await fetch(
+  "https://app.alfastore.co.id/prd/api/lpb/tablet/lpb/TotalFaktur/",
+  {
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json",
+
+      "App-Name": "LPB-CLOUD",
+      "Version-App": "V.2025.11.25.04",
+      "Version-Code": "30",
+
+      "User-Agent":
+        "Dalvik/2.1.0 (Linux; U; Android 15; Infinix X6885 Build/AP3A.240905.015.A2)",
+
+      "User-Id": "23067884",
+      "Store-Id": storeId,
+
+      "Api-Key":
+        "ivOZX9MLMKrjlL8R23uFlaryMRIvGMXG",
+
+      "AndroidId":
+        "712f8db18eeb1816",
+
+      "Platform": "ANDROID",
+
+      "Host":
+        "app.alfastore.co.id",
+
+      "Connection":
+        "Keep-Alive",
+
+      "Accept-Encoding":
+        "gzip"
+    },
 
 
-const ALFASTORE_URL =
-  "https://app.alfastore.co.id/prd/api/lpb/tablet/lpb/TotalFaktur/";
+    body: JSON.stringify({
+
+      storeId: storeId,
+
+      faktur: faktur
+
+    }),
 
 
-export async function GET(
-  request: NextRequest
-) {
-
-  try {
-
-    const { searchParams } =
-      new URL(request.url);
-
-
-    const storeId =
-      searchParams.get("storeId");
-
-    const faktur =
-      searchParams.get("faktur");
-
-
-    if (!storeId || !faktur) {
-
-      return NextResponse.json(
-        {
-          success: false,
-          message:
-            "storeId dan faktur wajib diisi"
-        },
-        {
-          status: 400
-        }
-      );
-
-    }
-
-
-    const apiUrl =
-      `${ALFASTORE_URL}` +
-      `?storeId=${encodeURIComponent(storeId)}` +
-      `&faktur=${encodeURIComponent(faktur)}`;
-
-
-
-    const response =
-      await fetch(apiUrl, {
-
-        method: "GET",
-
-        headers: {
-          "App-Name": "CEXP-CLOUD",
-          "Accept": "application/json"
-        },
-
-        cache: "no-store"
-
-      });
-
-
-
-    const data =
-      await response.text();
-
-
-
-    return new NextResponse(
-      data,
-      {
-        status: response.status,
-
-        headers: {
-          "Content-Type":
-            response.headers.get("content-type")
-            ??
-            "application/json",
-
-          "Cache-Control":
-            "no-store"
-        }
-      }
-    );
-
-
-
-  } catch (error) {
-
-
-    return NextResponse.json(
-      {
-        success:false,
-
-        message:
-          "Server error Total Faktur",
-
-        error:
-          error instanceof Error
-          ?
-          error.message
-          :
-          String(error)
-      },
-      {
-        status:500
-      }
-    );
-
-
+    cache: "no-store"
   }
-
-}
+);

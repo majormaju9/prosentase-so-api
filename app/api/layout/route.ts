e9daeb9da583c70cee4e30bbdca69b1455824f68
majormaju9 <majormaju9@gmail.com>
2026-08-23 16:08:07 +0700

    Update route.ts

--- app/api/layout/route.ts
+++ app/api/layout/route.ts
@@ -1,43 +1,53 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const HOPROIN_URL =
+const API_URL =
   "https://hoproin0201.sat.co.id/get/listphoto";
 
 
 export async function GET(request: NextRequest) {
+
   try {
 
     const { searchParams } = new URL(request.url);
 
     const storeId = searchParams.get("storeId") || "M604";
-    const userId = searchParams.get("userId") || "23067884";
-    const branchId = searchParams.get("branchId") || "MZ01";
 
 
-    const response = await fetch(HOPROIN_URL, {
+    const response = await fetch(API_URL, {
 
       method: "POST",
 
       headers: {
         "Content-Type": "application/json",
         "Accept": "application/json",
+
         "App-Name": "PROIN-PDA",
         "Version-App": "2025.08.25",
-        "User-Id": userId,
+
+        "User-Id": "23067884",
         "Store-Id": storeId,
+
         "Api-Key": "iVOZX9MLmKrj1L8R23uF1aryMR1vGMXG",
+
         "AndroidId": "712f8db18eeb1816",
-        "Branch-Id": branchId,
+
+        "Branch-Id": "MZ01",
         "Platform": "ANDROID",
+
       },
 
+
       body: JSON.stringify({
-        storeId,
-        userId,
-        branchId
+
+        storeId: storeId,
+
+        userId: "23067884",
+
+        branchId: "MZ01"
+
       }),
 
-      cache: "no-store"
+      cache:"no-store"
 
     });
 
@@ -45,45 +55,40 @@ export async function GET(request: NextRequest) {
     const text = await response.text();
 
 
-    if (!response.ok) {
+    if(!response.ok){
 
-      return NextResponse.json(
-        {
-          success:false,
-          status:response.status,
-          data:text
-        },
-        {
-          status:response.status
-        }
-      );
+      return NextResponse.json({
 
-    }
+        success:false,
+        status:response.status,
+        data:text
 
+      },{
+        status:response.status
+      });
 
-    let data;
-
-    try {
-      data = JSON.parse(text);
-    } catch {
-      data = text;
     }
 
 
     return NextResponse.json({
+
       success:true,
-      data
+      data:JSON.parse(text)
+
     });
 
 
   } catch(error:any){
 
     return NextResponse.json({
+
       success:false,
       message:error.message
+
     },{
       status:500
     });
 
   }
+
 }
